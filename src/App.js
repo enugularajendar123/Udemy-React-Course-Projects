6bfd3e0f3541e7552e2583e2a9efa7ccc9e06b2c
Enugula Rajendar <enugularajendar22@gmail.com>
2023-04-14 21:47:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
+import "./App.css";
 
 function App() {
+  const expenses = [
+    {
+      title: "travelling charges",
+      amount: 291,
+      expDate: new Date(2021,12,21).toISOString(),
+    },
+    {
+      title: "Insurance",
+      amount: 300,
+      expDate: new Date(2021,8,13).toISOString(),
+    },
+    {
+      title: "EMI",
+      amount: 100,
+      expDate: new Date(2021,9,5).toISOString(),
+    },
+    {
+      title: "Canteen",
+      amount: 156,
+      expDate: new Date(2023,10,10).toISOString(),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Expenses items={expenses} />
+
+      {/* <ExcerciseComp>{year}</ExcerciseComp> */}
     </div>
   );
 }
